ef0eab84068fdfa8ce4caac1d047aa74c51a8f9f
John McCambridge <jmccamb@gmail.com>
2020-08-04 19:09:03 -0500

    checkout

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.port || 3001
 const app = express()
 const sqlite3 = require('sqlite3').verbose()
 
+const inputCheck = require('./utils/inputCheck')
+
 // express middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -59,14 +61,35 @@ app.delete('/api/candidate/:id', (req, res) => {
     })
 })
 
-// CREATE a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`
-// const params = [1, 'Ronald', 'Firebank', 1]
-// db.run(sql, params, function (err, result) {
-//     err
-//         ? console.error(err)
-//         : console.info('Added candidate', result, this.lastID)
-// })
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(
+        body,
+        'first_name',
+        'last_name',
+        'industry_connected'
+    )
+    if (errors) {
+        res.status(400).json({ error: errors })
+        return
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`
+    const params = [body.first_name, body.last_name, body.industry_connected]
+    db.run(sql, params, function (err, _result) {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID,
+        })
+    })
+})
 
 // fallback for non-existant endpoint
 app.use((req, res) => {
